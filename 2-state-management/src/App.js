f47b56a42795a894ce292a4ca8d226cda75e6a0f
striderarun <strider90arun@gmail.com>
2025-06-30 14:54:54 -0700

    App.js and boilerplate

---
+++ 2-state-management/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import WrongModeToggler from './components/WrongModeToggler';
+import ModeToggler from './components/ModeToggler';
+import RegistrationForm from './components/RegistrationForm';
+import Calculator from './components/Calculator';
+
+const App = () => {
+  return (
+    //<WrongModeToggler/>
+    //<ModeToggler/>
+    //<RegistrationForm/>
+    <Calculator/>
+  );
+}
+
+export default App;
